d970dee5ceba5c94d67bf4a7e96150be8a71e3f6
marekstorek <marek.100rek@gmail.com>
2026-08-19 15:13:20 +0200

    add and train other CNN models with up to 5 layers

--- ml/models.py
+++ ml/models.py
@@ -16,3 +16,129 @@ class Conv1Layer(nn.Module):
         x = x.flatten(1)
         x = self.fco(x)
         return x
+
+class Conv2Layer(nn.Module):
+    def __init__(self, n1:int, n2:int, kernel_size: int = 3):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=n1, kernel_size=kernel_size, padding=kernel_size // 2)
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.conv2 = nn.Conv2d(in_channels=n1, out_channels=n2, kernel_size=kernel_size, padding=kernel_size // 2)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.fco = nn.Linear(n2 * (IMAGE_SIZE[0] // 4) * (IMAGE_SIZE[1] // 4), NUM_CLASSES)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = self.pool1(x)
+
+        x = F.relu(self.conv2(x))
+        x = self.pool2(x)
+        x = x.flatten(1)
+
+        x = self.fco(x)
+        return x
+
+class Conv3Layer(nn.Module):
+    def __init__(self, n1:int, n2:int, n3:int, kernel_size: int = 3):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=n1, kernel_size=kernel_size, padding=kernel_size // 2)
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.conv2 = nn.Conv2d(in_channels=n1, out_channels=n2, kernel_size=kernel_size, padding=kernel_size // 2)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.conv3 = nn.Conv2d(in_channels=n2, out_channels=n3, kernel_size=kernel_size, padding=kernel_size // 2)
+        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.fco = nn.Linear(n3 * (IMAGE_SIZE[0] // 8) * (IMAGE_SIZE[1] // 8), NUM_CLASSES)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = self.pool1(x)
+
+        x = F.relu(self.conv2(x))
+        x = self.pool2(x)
+
+        x = F.relu(self.conv3(x))
+        x = self.pool3(x)
+
+        x = x.flatten(1)
+        x = self.fco(x)
+        return x
+
+
+class Conv4Layer(nn.Module):
+    def __init__(self, n1: int, n2: int, n3: int, n4: int, kernel_size: int = 3):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=n1, kernel_size=kernel_size, padding=kernel_size // 2)
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.conv2 = nn.Conv2d(in_channels=n1, out_channels=n2, kernel_size=kernel_size, padding=kernel_size // 2)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.conv3 = nn.Conv2d(in_channels=n2, out_channels=n3, kernel_size=kernel_size, padding=kernel_size // 2)
+        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.conv4 = nn.Conv2d(in_channels=n3, out_channels=n4, kernel_size=kernel_size, padding=kernel_size // 2)
+        self.pool4 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.fco = nn.Linear(n4 * (IMAGE_SIZE[0] // 16) * (IMAGE_SIZE[1] // 16), NUM_CLASSES)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = self.pool1(x)
+
+        x = F.relu(self.conv2(x))
+        x = self.pool2(x)
+
+        x = F.relu(self.conv3(x))
+        x = self.pool3(x)
+
+        x = F.relu(self.conv4(x))
+        x = self.pool4(x)
+
+        x = x.flatten(1)
+        x = self.fco(x)
+        return x
+
+class Conv5Layer(nn.Module):
+    def __init__(self, n1: int, n2: int, n3: int, n4: int, n5: int, kernel_size: int = 3):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=n1, kernel_size=kernel_size, padding=kernel_size // 2)
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.conv2 = nn.Conv2d(in_channels=n1, out_channels=n2, kernel_size=kernel_size, padding=kernel_size // 2)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.conv3 = nn.Conv2d(in_channels=n2, out_channels=n3, kernel_size=kernel_size, padding=kernel_size // 2)
+        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.conv4 = nn.Conv2d(in_channels=n3, out_channels=n4, kernel_size=kernel_size, padding=kernel_size // 2)
+        self.pool4 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.conv5 = nn.Conv2d(in_channels=n4, out_channels=n5, kernel_size=kernel_size, padding=kernel_size // 2)
+        self.pool5 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.fco = nn.Linear(n5 * (IMAGE_SIZE[0] // 32) * (IMAGE_SIZE[1] // 32), NUM_CLASSES)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = self.pool1(x)
+
+        x = F.relu(self.conv2(x))
+        x = self.pool2(x)
+
+        x = F.relu(self.conv3(x))
+        x = self.pool3(x)
+
+        x = F.relu(self.conv4(x))
+        x = self.pool4(x)
+
+        x = F.relu(self.conv5(x))
+        x = self.pool5(x)
+
+        x = x.flatten(1)
+        x = self.fco(x)
+        return x
+
